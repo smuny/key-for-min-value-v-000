67cb0ddc51ec169ccbe1e4b1608761d481751998
Sharon Mun <sharonymun@gmail.com>
2018-06-29 21:44:29 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,24 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  low_value = nil
+  hash_value = nil
+  name_hash.collect do |element, value|
+    if value = hash_value
+      element = low_value
+    end
+  end
+ low_value
+end
 
+# birthday_kids = {
+#   "Timmy" => 9, 
+#   "Sarah" => 6, 
+#   "Amanda" => 27
+# }
+
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
